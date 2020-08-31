6eaecf508fb24bdc594811d24fd95acf6b16cd52
Yeseul O <yeseulo@gmail.com>
2020-08-31 19:10:19 +0900

    Updates

---
+++ precache-manifest.a6a5530236d4a862421611b7060d7d31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0ae8f666176434a1981d656c9ab5eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ca8c9b4c7b996be087b",
+    "url": "/static/js/2.634961d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.634961d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8e682b9e48a18e627b",
+    "url": "/static/js/main.83f00c92.chunk.js"
+  },
+  {
+    "revision": "c84b4117421d53d88033",
+    "url": "/static/js/runtime-main.7e4cedf3.js"
+  },
+  {
+    "revision": "3652d7aa0c18018a40a804144738a096",
+    "url": "/static/media/OSGroteskWeb-Regular.3652d7aa.ttf"
+  },
+  {
+    "revision": "a46d64f6273dcb49d97992fec6b5c32f",
+    "url": "/static/media/OSGroteskWeb-Regular.a46d64f6.woff2"
+  },
+  {
+    "revision": "a4d33fb8434f8ae86ea3997811a4fff7",
+    "url": "/static/media/OSGroteskWeb-Regular.a4d33fb8.eot"
+  },
+  {
+    "revision": "eee8417cfe7677818f7e3750649b2f72",
+    "url": "/static/media/OSGroteskWeb-Regular.eee8417c.woff"
+  }
+]);
